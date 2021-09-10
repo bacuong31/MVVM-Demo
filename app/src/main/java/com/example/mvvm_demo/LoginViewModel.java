c023553a0ae83f3eb03b4f75f7cdca2fa26a3b70
bacuong312 <bacuong312@gmail.com>
2021-09-10 11:04:29 +0700

    mvvm-demo

---
+++ app/src/main/java/com/example/mvvm_demo/LoginViewModel.java
@@ -0,0 +1,40 @@
+package com.example.mvvm_demo;
+
+import androidx.databinding.BaseObservable;
+import androidx.databinding.Bindable;
+import androidx.databinding.ObservableField;
+
+import com.example.mvvm_demo.BR;
+import com.example.mvvm_demo.Model.User;
+
+public class LoginViewModel extends BaseObservable {
+    private String email;
+    private String password;
+    public ObservableField<String> messageLogin = new ObservableField<>();
+    @Bindable
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+        notifyPropertyChanged(BR.email);
+    }
+    @Bindable
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+        notifyPropertyChanged(BR.password);
+    }
+    public void onClickLogin(){
+        User user = new User(getEmail(),getPassword());
+        if(user.isValidEmail() && user.isValidPassword()){
+            messageLogin.set("Login success!");
+        } else {
+            messageLogin.set("Email or Password is invalid!");
+        }
+    }
+}
